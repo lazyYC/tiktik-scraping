4b27903e518e69bd8370a840abbebed3d89a7284
lazyYC <yuchilin0515@gmail.com>
2024-03-05 16:04:18 +0800

    first commit

---
+++ crawler/crawler.py
@@ -0,0 +1,95 @@
+import random
+import time
+from datetime import datetime, timedelta
+import json
+from undetected_chromedriver import Chrome, ChromeOptions
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+from urllib.parse import urlparse, parse_qs
+
+# TODO: exception handling (retry, timeout, etc.)
+# TODO: logging
+
+class TiktokCrawler:
+    
+    def __init__(self, channel_name: str):
+        self.channel_url = f'https://www.tiktok.com/@{channel_name}'
+        self.options = ChromeOptions()
+        # self.options.add_argument("--headless")
+        self.options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
+        self.driver = Chrome(options=self.options)
+
+    def exec_crawler(self):
+        self.visit_main_page()
+        oldest_post_time = self.get_oldest_post_time()
+        urls = self.scroll_page_handler(oldest_post_time)
+        self.requests_api(urls)
+        self.driver.quit()
+
+    def visit_main_page(self):
+        self.driver.get(self.channel_url)
+        guest_btn = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[class*="DivGuestModeContainer"]'))
+            )
+        guest_btn.click()
+        
+    def get_oldest_post_time(self) -> datetime:
+        return datetime.now() - timedelta(days=2)
+        
+    def scroll_page_handler(self, oldest_post_time: datetime):
+        urls = []
+        keep_scrolling = True
+        while keep_scrolling:
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+            if url := self.parse_urls_from_logs():
+                urls.extend(url)
+                print(type(urls[-1]))
+                cursor = float(parse_qs(urlparse(urls[-1]).query).get('cursor', [''])[0])
+                if datetime.fromtimestamp(cursor/1000) < oldest_post_time:
+                    keep_scrolling = False
+                    
+            else:
+                continue
+        return urls
+        
+    def parse_urls_from_logs(self) -> list:
+        logs = self.driver.get_log('performance')
+        urls = []
+        for entry in logs:
+            log = json.loads(entry["message"])["message"]
+            if "Network.requestWillBeSent" == log["method"]:
+                url = log["params"]["request"]["url"]
+                if "item_list" in url and url not in urls:
+                    urls.append(url)
+        return urls
+
+    def requests_api(self, urls: list):
+        for url in urls:
+            time.sleep(random.randint(1, 3))
+            self.driver.get(url)
+            stats = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "pre"))
+            )
+
+            response = json.loads(stats.text)
+            
+            for post in response["itemList"]:
+                # if datetime.fromtimestamp(post["createTime"]) < oldest_post_time:
+                #    break
+                data_to_write = dict(
+                    tiktok_id = post["id"],
+                    title = post["desc"],
+                    collect_count = int(post["stats"]["collectCount"]),
+                    digg_count =int( post["stats"]["diggCount"]),
+                    share_count = int(post["stats"]["shareCount"]),
+                    comment_count = int(post["stats"]["commentCount"]),
+                    play_count = int(post["stats"]["playCount"]),
+                    created_time = datetime.fromtimestamp(post["createTime"])
+                )
+                # TODO: write data_to_write to db
+                print(data_to_write)
---
+++ main.py
@@ -0,0 +1,6 @@
+# from crawler import crawler
+
+# if __name__ == "__main__":
+#     channel_name = "geevideo"
+#     crawler = crawler.TiktokCrawler(channel_name)
+#     crawler.exec_crawler()
