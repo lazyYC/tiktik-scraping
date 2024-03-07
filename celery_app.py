8f7e5c9004e7dbf59bade963c916030feea382a0
lazyYC <yuchilin0515@gmail.com>
2024-03-08 01:21:37 +0800

    celery_app connection settings

--- celery_app.py
+++ celery_app.py
@@ -1,13 +1,13 @@
 from celery import Celery
 from celery.schedules import crontab
 
-app = Celery('tiktok_scraping', broker='redis://redis:6379/0')
-app.conf.result_backend = 'redis://redis:6379/0'
+app = Celery('tiktok_scraping', broker='redis://localhost:6379/0')
+app.conf.result_backend = 'redis://localhost:6379/0'
 app.autodiscover_tasks(['crawler.tasks'], force=True)
 app.conf.beat_schedule = {
     'exec-crawler-every-hour': {
         'task': 'crawler.tasks.exec_crawler_task',  
-        'schedule': crontab(minute='*/1'),
+        'schedule': crontab(minute='*/5'),
     },
 }
 
